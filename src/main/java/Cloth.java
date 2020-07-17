768e453eb72a50ecd33486678f12a34d77856827
akieliszczyk <38585186+akieliszczyk@users.noreply.github.com>
2020-07-17 08:19:08 +0200

    Create Cloth.java

---
+++ src/main/java/Cloth.java
@@ -0,0 +1,40 @@
+public class Cloth {
+
+    private String typeOfCloth;
+    private Integer completionDate;
+    private Integer size;
+    private String company;
+    
+
+    public String getTypeOfCloth() {
+        return typeOfCloth;
+    }
+
+    public void setTypeOfCloth(String typeOfCloth) {
+        this.typeOfCloth = typeOfCloth;
+    }
+
+    public Integer getDate() {
+        return completionDate;
+    }
+
+    public void setCompletionDate(Integer completionDate) {
+        this.completionDate = completionDate;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+}
