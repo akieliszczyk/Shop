f68d70b6d571018ad673869a7a6b5cc1f67e38be
akieliszczyk <38585186+akieliszczyk@users.noreply.github.com>
2020-07-18 13:22:41 +0200

    Create ListOfUsers.java
    Methods to create users

---
+++ src/main/java/randomUserData/ListOfUsers.java
@@ -0,0 +1,31 @@
+package randomUserData;
+
+import com.github.javafaker.Faker;
+
+import java.util.List;
+
+public class ListOfUsers {
+    UserData userData = new UserData();
+    Faker faker = new Faker();
+
+    private Object users[][] = new Object[20][3];
+
+    public Object create20users() {
+
+        for(int i = 0; i<20; i++) {
+
+            userData.setFirstName(faker.name().firstName());
+            userData.setSecondName(faker.name().lastName());
+            userData.setFullName(userData.getFirstName() + " " + userData.getSecondName());
+            userData.setAge(faker.number().numberBetween(1, 100));
+            userData.setEmail(userData.getFirstName() + "." + userData.getSecondName() + "@google.pl");
+
+            users[i][0] = userData.getFullName();
+            users[i][1] = userData.getAge();
+            users[i][2] = userData.getEmail();
+        }
+        return users;
+    }
+
+
+}
