8a03654c76ed299af4c86dc53df15ab9f97f8a13
akieliszczyk <kurszewska.anna@gmail.com>
2022-07-04 12:14:07 +0200

    Add WebDriverManager

---
+++ src/test/java/config/BaseTest.java
@@ -0,0 +1,23 @@
+package config;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utility.Constants;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @Before
+    public void openPage() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(Constants.shopURL);
+    }
+}
